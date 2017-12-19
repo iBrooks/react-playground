747be1d4bf4ac96b9cf2af318d41ba08cec5a2b3
Ian Launcher <ianlauncher@Ians-MacBook-Pro.local>
2017-12-19 11:44:33 -0500

    Added a load of crap that doesnt work. Deprecating this branch

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Child1 from './Child1'
+import Foundation from 'react-foundation'
+import Button from './Button'
+
 
 const Parent = props => {
   return(
+    <div>
+    <Button color={Colors.SUCCESS}>Save</Button>
     <Child1 />
+    </div>
   )
 }
 export default Parent
