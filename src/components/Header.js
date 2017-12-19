bde9f8850910bc723bda6a6cf0ad3485a93ed34b
Ian Launcher <ianlauncher@Ians-MacBook-Pro.local>
2017-12-19 10:15:49 -0500

    Added foundation files. Totally broken

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Header = props =>{
+return(
+  // <div class="row">
+  //   <div class="large-12 columns">
+      <h1>Welcome to Ian's React + Foundation Playground</h1>
+    // </div>
+  // </div>
+)
+}
+export default Header
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Child1 from './Child1'
+import Header from './Header'
 
 const Parent = props => {
   return(
-    <Child1 />
+    <div>
+      <Header />
+      <Child1 />
+    </div>
   )
 }
 export default Parent
